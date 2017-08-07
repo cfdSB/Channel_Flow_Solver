6148497115f5858c6d3043309e45b21aec8db78c
cfdSB <vishvambharvitthal@gmail.com>
2017-08-07 10:24:44 -0400

    Updated main class to reflect changes in the rest of the code.

--- Channel_Flow_Solver/main.cpp
+++ Channel_Flow_Solver/main.cpp
@@ -24,6 +24,8 @@
 #include "PhysicsContinuum.h"
 #include "MatrixSolver.h"
 #include "Material.h"
+#include "SolutionManager.h"
+#include "BoundaryManager.h"
 
 using namespace std;
 
@@ -50,24 +52,62 @@ int main(int argc, char** argv) {
     pc.setMaterial(&air);
     
     VolumeMesh* mesh = mesher.getVolumeMesh();
-    std::unique_ptr<MeshDescritizer> discretizer(new MeshDescritizer(mesh, &pc));
-//    discretizer->setConvectionDifferencingScheme(MeshDescritizer::ConvectionDifferencingScheme::CENTRAL);
-//    discretizer->setsimulationType(MeshDescritizer::SimulationType::DIFFUSION);
-    discretizer->computeDiscretizationCoefficients();
-    discretizer->printCoefficients();
+    
+    std::unique_ptr<BoundaryManager> bndManager(new BoundaryManager(mesh));
+    Boundary *bnd_xm = bndManager->createBoundary("xm", "x", xMin);
+    Boundary *bnd_xp = bndManager->createBoundary("xp", "x", xMax);
+    Boundary *bnd_ym = bndManager->createBoundary("ym", "y", yMin);
+    Boundary *bnd_yp = bndManager->createBoundary("yp", "y", yMax);
+    Boundary *bnd_zm = bndManager->createBoundary("zm", "z", zMin);
+    Boundary *bnd_zp = bndManager->createBoundary("zp", "z", zMax);
+    
+    std::unique_ptr<SolutionManager> solManager (new SolutionManager(mesh));
+    SolutionVariable* vel_i = solManager->createSolutionVariable("i_velocity", SolutionVariable::variableType::FIXED_FIELD);
+    vel_i->initializeVariable(30.0);
+    SolutionVariable* vel_j = solManager->createSolutionVariable("j_velocity", SolutionVariable::variableType::FIXED_FIELD);
+    vel_j->initializeVariable(10.0);
+    SolutionVariable* vel_k = solManager->createSolutionVariable("k_velocity", SolutionVariable::variableType::FIXED_FIELD);
+    vel_k->initializeVariable(10.0);
+    SolutionVariable* temperature = solManager->createSolutionVariable("Temperature", SolutionVariable::variableType::SOLUTION_FIELD);
+    temperature->initializeVariable(0.0);
+
+    //debug statements
+    std::vector<FvCell*> *cells = mesh->getCells();
+    for (size_t i = 0; i < cells->size(); i++) {
+        double val1 = *(cells->at(i)->getSolutionField("i_velocity"));
+        double val2 = *(cells->at(i)->getSolutionField("j_velocity"));
+        std::cout << val1 << ", " << val2 << std::endl;
+    }
     
     std::unique_ptr<BoundaryConditionsManager> bcManager (new BoundaryConditionsManager(mesh));
-    //std::string plane = "x"; double coord = 0.0; double tolerance = 1.0e-6; double bcValue = 500.00;
-    std::string plane = "x"; double coord = 0.0; double tolerance = 1.0e-6; double bcValue = 500.00;
-    bcManager->createBoundaryCondition(plane, coord, tolerance, BoundaryCondition::BcType::FIXED_FLUX, bcValue);
-    std::string plane2 = "z"; double coord2 = 0.0; double tolerance2 = 1.0e-6; double bcValue2 = 0.00;
-    bcManager->createBoundaryCondition(plane2, coord2, tolerance2, BoundaryCondition::BcType::ADIABATIC, bcValue2);
-    bcManager->createBoundaryCondition("x", 0.3, 1.0e-6, BoundaryCondition::BcType::FIXED_VALUE,300.00);
-    bcManager->createBoundaryCondition("y", 0.0, 1.0e-6, BoundaryCondition::BcType::ADIABATIC,0.0);
-    bcManager->createBoundaryCondition("y", 0.3, 1.0e-6, BoundaryCondition::BcType::ADIABATIC,0.0);
-    bcManager->createBoundaryCondition("z", 0.3, 1.0e-6, BoundaryCondition::BcType::ADIABATIC,0.0);
-    bcManager->printBoundaryConditionsReport();
-  
+    BoundaryCondition* bc_xm = bcManager->createBoundaryCondition(temperature, BoundaryCondition::BcType::FIXED_FLUX, 500.00);
+    BoundaryCondition* bc_zm = bcManager->createBoundaryCondition(temperature, BoundaryCondition::BcType::ADIABATIC, 0.0);
+    BoundaryCondition* bc_xp = bcManager->createBoundaryCondition(temperature, BoundaryCondition::BcType::FIXED_VALUE,300.00);
+    BoundaryCondition* bc_ym = bcManager->createBoundaryCondition(temperature, BoundaryCondition::BcType::ADIABATIC,0.0);
+    BoundaryCondition* bc_yp = bcManager->createBoundaryCondition(temperature, BoundaryCondition::BcType::ADIABATIC,0.0);
+    BoundaryCondition* bc_zp = bcManager->createBoundaryCondition(temperature, BoundaryCondition::BcType::ADIABATIC,0.0);
+    BoundaryCondition* bc_vel1_xm = bcManager->createBoundaryCondition(vel_i, BoundaryCondition::BcType::FIXED_VALUE, 0.1);
+    BoundaryCondition* bc_vel1_xp = bcManager->createBoundaryCondition(vel_i, BoundaryCondition::BcType::FIXED_VALUE, 0.1);
+    BoundaryCondition* bc_vel1_rest = bcManager->createBoundaryCondition(vel_i, BoundaryCondition::BcType::FIXED_VALUE, 0.0);
+    BoundaryCondition* bc_vel2_rest = bcManager->createBoundaryCondition(vel_j, BoundaryCondition::BcType::FIXED_VALUE, 0.0);
+    BoundaryCondition* bc_vel3_rest = bcManager->createBoundaryCondition(vel_k, BoundaryCondition::BcType::FIXED_VALUE, 0.0);
+
+
+
+    bcManager->printBoundaryConditionsReport();    
+    
+    bnd_xm->addBoundaryCondition(bc_xm); bnd_xm->addBoundaryCondition(bc_vel1_xm); bnd_xm->addBoundaryCondition(bc_vel2_rest); bnd_xm->addBoundaryCondition(bc_vel3_rest);
+    bnd_xp->addBoundaryCondition(bc_xp); bnd_xp->addBoundaryCondition(bc_vel1_xp); bnd_xp->addBoundaryCondition(bc_vel2_rest); bnd_xp->addBoundaryCondition(bc_vel3_rest);
+    bnd_ym->addBoundaryCondition(bc_ym); bnd_ym->addBoundaryCondition(bc_vel1_rest); bnd_ym->addBoundaryCondition(bc_vel2_rest); bnd_ym->addBoundaryCondition(bc_vel3_rest);
+    bnd_yp->addBoundaryCondition(bc_yp); bnd_yp->addBoundaryCondition(bc_vel1_rest); bnd_yp->addBoundaryCondition(bc_vel2_rest); bnd_yp->addBoundaryCondition(bc_vel3_rest);
+    bnd_zm->addBoundaryCondition(bc_zm); bnd_zm->addBoundaryCondition(bc_vel1_rest); bnd_zm->addBoundaryCondition(bc_vel2_rest); bnd_zm->addBoundaryCondition(bc_vel3_rest);
+    bnd_zp->addBoundaryCondition(bc_zp); bnd_zp->addBoundaryCondition(bc_vel1_rest); bnd_zp->addBoundaryCondition(bc_vel2_rest); bnd_zp->addBoundaryCondition(bc_vel3_rest);
+    
+    
+    std::unique_ptr<MeshDescritizer> discretizer(new MeshDescritizer(mesh, &pc));
+    discretizer->computeDiscretizationCoefficients();
+    discretizer->printCoefficients();
+   
     discretizer->updateCoefficients(bcManager->getBoundaryConditions());
     discretizer->printCoefficients();
     
